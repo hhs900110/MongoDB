2f8617bdf2f32525417ca80c6a1850003f6b0082
hshan <hhs900110@gmail.com>
2016-06-16 18:11:49 +0900

    Singleton 추가

---
+++ MongoDBTestProject/Source/Basic/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Singleton<TClass>
+    where TClass : class, new()
+{
+    private static TClass mInstance;
+    public static TClass Instance
+    {
+        get
+        {
+            if ( mInstance == null )
+            {
+                mInstance = new TClass();
+            }
+            return mInstance;
+        }
+    }
+}
